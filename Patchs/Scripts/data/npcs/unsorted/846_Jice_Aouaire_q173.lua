89417dfaca5820a42b3f0490ce2b9e9ee410bd8c
Nairelos <Nairelos@hotmail.fr>
2023-05-10 19:01:47 +0200

    update

--- Patchs/Scripts/data/npcs/unsorted/846_Jice_Aouaire_q173.lua
+++ Patchs/Scripts/data/npcs/unsorted/846_Jice_Aouaire_q173.lua
@@ -2,25 +2,10 @@ local npc = Npc(846, 80)
 local questID = 173
 
 npc.colors = {16760576, 13055806, 10627125}	
-npc.accessories = {1fa3, 275, 953, 0, 1ba8}
+npc.accessories = {0x1fa3, 0x275, 0x953, 0, 0x1ba8}
 npc.customArwork = 9087
 
 npc.quests = {questID}
-
---Quest_data : 173
---Quest_objective 1 : 720 (Rapporter X Laine de bouftou (idItem :384))
---Quest_Objective 2 : 750 (Visiter la maps 10307, la milice kerubim)
---Quest_step : 
-
---Q.3567 > R.3151 > 
---		   R.3150 > Q.3568 > R.3152 > Q.3569 > R.3154
---						     R.3153
-
---Quest Ongoing : Q.3678
---Quete Finish : Q.3570
---ASK Update Objective 750 : 3685
---ASK Reception Objective 720 : 3571
-
 
 ---@param p Player
 ---@param answer number
@@ -39,24 +24,26 @@ function npc:onTalk(p, answer)
 			end
 			return
 		end
-		--On valide l'objectif lorsque l'on donne 3 Laine de Bouftou 	
+ 	
 		if quest:ongoingFor(p) then
+		-- If we have the required items, complete objective
 			if p:consumeItem(384, 3) then 
 				quest:completeObjective(p, 720) 
-				p:ask(3678)--Question a affiché quand l'objectif n'est pas encore valider
+				p:ask(3571) 
 				return
 			end
-			-- Question a affiché quand l'objectif est validé
-			p:ask(3571)
-			p:endDialog()
+			-- Ongoing quest dialog
+			p:ask(3678)
 			return
 		end
-		-- On reparle au npc pour que la question nous donne l'objectif 750 
-		p:ask(3685) 
+		-- Update quest for next step
+		if quest:completeObjectives (p, 332) then
+			if answer==0 then
+				p:ask(3685) 
 		if quest:ongoingFor(p) then
 			if p:mapID(10307) then
 				quest:completeObjective(p, 750)
-				p:ask(3678)
+				p:ask(3570)
 				return
 			end
 			return
--- Patchs/Scripts/data/quests/incarnam/173_SoyezAouaire.lua
+++ Patchs/Scripts/data/quests/incarnam/173_SoyezAouaire.lua
@@ -1,8 +1,13 @@
 local qs332 = QuestStep(332, 3569)
-local q173 = Quest(173, {qs332})
+local qs346 = QuestStep(346, 3685)
+local q173 = Quest(173, {qs332, qs346})
 
 qs332.objectives = q173:SequentialObjectives({
 	BringItemObjective(720, 846, 384, 3)
-	DiscoverMapObjective = (750, 10307)
 })
 qs332.rewardFn = QuestBasicReward(120, 150)
+
+qs346.objectives = q173:SequentialObjectives({
+	DiscoverMapObjective = (750, 10307)
+})
+qs346.rewardFn = QuestBasicReward(25, 0)
