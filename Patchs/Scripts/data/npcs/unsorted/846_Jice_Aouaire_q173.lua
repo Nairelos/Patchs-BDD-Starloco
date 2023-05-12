b687f182c5bd2e40f6920e45079b169b75c69ef2
Nairelos <Nairelos@hotmail.fr>
2023-05-12 18:19:27 +0200

    update

--- Patchs/Scripts/data/npcs/unsorted/846_Jice_Aouaire_q173.lua
+++ Patchs/Scripts/data/npcs/unsorted/846_Jice_Aouaire_q173.lua
@@ -3,57 +3,53 @@ local questID = 173
 
 npc.colors = {16760576, 13055806, 10627125}	
 npc.accessories = {0x1fa3, 0x275, 0x953, 0, 0x1ba8}
-npc.customArwork = 9087
+npc.customArtwork = 9087
 
 npc.quests = {questID}
 
 ---@param p Player
 ---@param answer number
-
 function npc:onTalk(p, answer)
 	local quest = QUESTS[questID]
 		
-		if quest:availableTo(p) then
-			if answer == 0 then 
-				p:ask(3567, {3151, 3150})
-			elseif answer == 3150 then p:ask(3668, {3152, 3153})
-			elseif answer == 3152 then p:ask(3569, {3154})
-			elseif answer == 3154 then
-				quest:startFor(p, self.id)
-				p:endDialog()
-			end
-			return
+	if quest:availableTo(p) then
+		if answer == 0 then
+			p:ask(3567, {3151, 3150})
+		elseif answer == 3150 then p:ask(3668, {3152, 3153})
+		elseif answer == 3152 then p:ask(3569, {3154})
+		elseif answer == 3154 then
+			quest:startFor(p, self.id)
+			p:endDialog()
 		end
- 	
-		if quest:ongoingFor(p) then
-		-- If we have the required items, complete objective
-			if p:consumeItem(384, 3) then 
-				quest:completeObjective(p, 720) 
-				p:ask(3571) 
-				return
-			end
-			-- Ongoing quest dialog
-			p:ask(3678)
-			return
-		end
-		-- Update quest for next step
-		if quest:completeObjectives (p, 332) then
-			if answer==0 then
-				p:ask(3685) 
-		if quest:ongoingFor(p) then
-			if p:mapID(10307) then
-				quest:completeObjective(p, 750)
-				p:ask(3570)
-				return
-			end
-			return
-		end
-				
-		if quest:finishedBy(p) then
-			p:ask(3570)
-			return
-		end
-		p:endDialog()
+		return
 	end
+ 	
+	if quest:ongoingFor(p) then
+		--- If we have the required items
+		if p:consumeItem(384, 3) then
+			quest:completeObjective(p, 720)
+			p:ask(3571)
+			return
+		end
+		--- Ongoing quest dialog
+		p:ask(3678)
+		return
+		--- If we have visited the map
+		---
+		---if QuestStep:ObjectivesForPlayer(p, 346)
+		---quest:completeObjective(p, 750)
+
+		---end
+		--- Ongoing quest dialog
+		---p:ask(3685)
+		---return
+	end
+
+	if quest:finishedBy(p) then
+		p:ask(3570)
+		return
+	end
+	p:endDialog()
+end
 
 RegisterNPCDef(npc)
--- Patchs/Scripts/data/quests/incarnam/173_SoyezAouaire.lua
+++ Patchs/Scripts/data/quests/incarnam/173_SoyezAouaire.lua
@@ -1,13 +1,15 @@
+local npcId = 846
+
 local qs332 = QuestStep(332, 3569)
 local qs346 = QuestStep(346, 3685)
 local q173 = Quest(173, {qs332, qs346})
 
-qs332.objectives = q173:SequentialObjectives({
+qs332.objectives = {
 	BringItemObjective(720, 846, 384, 3)
-})
+}
 qs332.rewardFn = QuestBasicReward(120, 150)
 
-qs346.objectives = q173:SequentialObjectives({
-	DiscoverMapObjective = (750, 10307)
-})
+qs346.objectives = {
+	DiscoverMapObjective(750, 10307)
+}
 qs346.rewardFn = QuestBasicReward(25, 0)
